26eaf799f982987ccbce575eccf16e41e6161e0d
Savelii Korniiko <saveliykorniko@gmail.com>
2026-02-15 13:53:44 +0200

    add parking data

--- agent/src/file_datasource.py
+++ agent/src/file_datasource.py
@@ -1,22 +1,27 @@
 import csv
 from random import randint
 from datetime import datetime
+from domain.parking import Parking
 from domain.accelerometer import Accelerometer
 from domain.gps import Gps
 from domain.aggregated_data import AggregatedData
 
 class FileDatasource:
-    def __init__(self, accelerometer_filename: str, gps_filename: str, user_id: int = 0) -> None:
+    def __init__(self, accelerometer_filename: str, gps_filename: str, parking_filename: str, user_id: int) -> None:
         self.acc_filename = accelerometer_filename
         self.gps_filename = gps_filename
+        self.parking_filename = parking_filename
         self.user_id = user_id
         self.acc_file = None
         self.gps_file = None
+        self.parking_file = None
         self.acc_reader = None
         self.gps_reader = None
+        self.parking_reader = None
         # Headers csv must have
         self._required_acc = {'x', 'y', 'z'}
         self._required_gps = {'longitude', 'latitude'}
+        self._required_parking = {'empty_count', 'longitude', 'latitude'}
 
     def _verify_headers(self, reader, required_fields, filename):
         if not reader.fieldnames:
@@ -36,21 +41,25 @@ class FileDatasource:
         self.gps_reader = csv.DictReader(self.gps_file)
         self._verify_headers(self.gps_reader, self._required_gps, self.gps_filename)
 
-    def _read_single(self):
-        try:
-            acc_row = next(self.acc_reader)
-            gps_row = next(self.gps_reader)
-        except StopIteration:
-            # Infinite cycle logic 
-            self.acc_file.seek(0)
-            self.gps_file.seek(0)
-            self.acc_reader = csv.DictReader(self.acc_file)
-            self.gps_reader = csv.DictReader(self.gps_file)
-            next(self.acc_reader) # Skip header row on re-read
-            next(self.gps_reader)
-            acc_row = next(self.acc_reader)
-            gps_row = next(self.gps_reader)
+        self.parking_file = open(self.parking_filename, 'r')
+        self.parking_reader = csv.DictReader(self.parking_file)
+        self._verify_headers(self.parking_reader, self._required_parking, self.parking_filename)
 
+    def __cycling_next(self, reader_attr_name, file):
+        reader = getattr(self, reader_attr_name)
+        try:
+            res = next(reader)
+        except StopIteration:
+            file.seek(0)
+            reader = csv.DictReader(file)
+            next(reader) # Skip header row on re-read
+            res = next(reader)
+            setattr(self, reader_attr_name, reader)
+        return res
+
+    def _read_single_agg(self) -> AggregatedData:
+        acc_row = self.__cycling_next('acc_reader', self.acc_file)
+        gps_row = self.__cycling_next('gps_reader', self.gps_file)
         return AggregatedData(
             Accelerometer(int(acc_row['x']), int(acc_row['y']), int(acc_row['z'])),
             Gps(float(gps_row['longitude']), float(gps_row['latitude'])),
@@ -58,9 +67,19 @@ class FileDatasource:
             self.user_id
         )
 
-    def read(self) -> AggregatedData:
-        return [self._read_single() for _ in range(randint(5, 7))]
+    def _read_single_park(self) -> Parking:
+        parking_row = self.__cycling_next('parking_reader', self.parking_file)
+        return Parking(
+            int(parking_row["empty_count"]),
+            Gps(float(parking_row['longitude']), float(parking_row['latitude'])))
+
+    def read(self) -> tuple[list[AggregatedData],list[Parking]]:
+        return (
+            [self._read_single_agg() for _ in range(randint(5, 7))],
+            [self._read_single_park() for _ in range(randint(5, 7))]
+            )
 
     def stopReading(self):
         if self.acc_file: self.acc_file.close()
         if self.gps_file: self.gps_file.close()
+        if self.parking_file: self.parking_file.close()
--- agent/src/main.py
+++ agent/src/main.py
@@ -1,3 +1,4 @@
+from schema.parking_schema import ParkingSchema
 from paho.mqtt import client as mqtt_client
 import json
 import time
@@ -24,29 +25,31 @@ def connect_mqtt(broker, port):
     return client
 
 
-def publish(client, topic, datasource, delay):
+def publish(client, topicAgg, topicPark, datasource: FileDatasource, delay):
     datasource.startReading()
     while True:
         time.sleep(delay)
-        data = datasource.read()
-        msg = AggregatedDataSchema(many=True).dumps(data)
-        result = client.publish(topic, msg)
-        # result: [0, 1]
-        status = result[0]
-        if status == 0:
-            pass
-            # print(f"Send `{msg}` to topic `{topic}`")
-        else:
-            print(f"Failed to send message to topic {topic}")
+        aggData, parking = datasource.read()
+        results = []
+        results.append((client.publish(topicAgg, AggregatedDataSchema(many=True).dumps(aggData)), topicAgg))
+        results.append((client.publish(topicPark, ParkingSchema(many=True).dumps(parking)), topicPark))
+        for result, topic in results:
+            # result: [0, 1]
+            status = result[0]
+            if status == 0:
+                pass
+                # print(f"Send `{msg}` to topic `{topic}`")
+            else:
+                print(f"Failed to send message to topic {topic}")
 
 
 def run():
     # Prepare mqtt client
     client = connect_mqtt(config.MQTT_BROKER_HOST, config.MQTT_BROKER_PORT)
     # Prepare datasource
-    datasource = FileDatasource("data/accelerometer.csv", "data/gps.csv")
+    datasource = FileDatasource("data/accelerometer.csv", "data/gps.csv", "data/parking.csv", config.USER_ID)
     # Infinity publish data
-    publish(client, config.MQTT_TOPIC, datasource, config.DELAY)
+    publish(client, config.MQTT_TOPIC_AGG, config.MQTT_TOPIC_PARK, datasource, config.DELAY)
 
 
 if __name__ == "__main__":
